fefdba85f2d3012d97ee7d7947e12f7fafb28c72
张鹤潇 <zhanghx18@mails.tsinghua.edu.cn>
2021-03-02 14:20:29 +0800

    根据指导书要求更新目录结构

--- build.rs
+++ build.rs
@@ -7,10 +7,10 @@ fn main() {
     insert_app_data().unwrap();
 }
 
-static TARGET_PATH: &str = "../user/target/riscv64gc-unknown-none-elf/release/";
+static TARGET_PATH: &str = "../user/build/bin";
 
 fn insert_app_data() -> Result<()> {
-    let mut f = File::create("src/link_app.S").unwrap();
+    let mut f = File::create("os/src/link_app.S").unwrap();
     let mut apps: Vec<_> = read_dir("../user/src/bin")
         .unwrap()
         .into_iter()
