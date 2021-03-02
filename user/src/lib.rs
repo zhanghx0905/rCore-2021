cd316f8eb5523e073c33b718d511bd8e49c0f6b5
张鹤潇 <zhanghx18@mails.tsinghua.edu.cn>
2021-03-02 12:13:33 +0800

    update test cases

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -2,16 +2,36 @@
 #![feature(llvm_asm)]
 #![feature(linkage)]
 #![feature(panic_info_message)]
+#![feature(alloc_error_handler)]
 
 #[macro_use]
 pub mod console;
 mod syscall;
 mod lang_items;
 
+use buddy_system_allocator::LockedHeap;
+
+pub const STDOUT: usize = 1;
+const USER_HEAP_SIZE: usize = 16384;
+
+static mut HEAP_SPACE: [u8; USER_HEAP_SIZE] = [0; USER_HEAP_SIZE];
+
+#[global_allocator]
+static HEAP: LockedHeap = LockedHeap::empty();
+
+#[alloc_error_handler]
+pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
+    panic!("Heap allocation error, layout = {:?}", layout);
+}
+
 #[no_mangle]
 #[link_section = ".text.entry"]
 pub extern "C" fn _start() -> ! {
     clear_bss();
+    unsafe {
+        HEAP.lock()
+            .init(HEAP_SPACE.as_ptr() as usize, USER_HEAP_SIZE);
+    }
     exit(main());
     panic!("unreachable after sys_exit!");
 }
