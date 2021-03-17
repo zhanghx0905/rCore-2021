bfa0f8d3de5359907ff6e072d6655987014a503a
zhanghx0905 <731931282@qq.com>
2021-03-17 15:12:56 +0800

    完成usershell

--- README.md
+++ README.md
@@ -2,13 +2,31 @@
 
 2021 Spring, OS Assignment.
 
-### dev-ch8 TODO LIST
+### dev-ch8
 
-- [x] 改写用户模块，以移植 tutorial 和 testcases 仓库内的所有用户程序。
+- [x] 在 testcases 的基础上修改用户模块，以移植 tutorial 仓库 ch7 分支上的的用户程序。
 
-- [x] 修改 `spawn` 接口，传递参数。
+- [x] 修改 `spawn` 接口，支持参数传递。
 
-- [ ] 重写 `user_shell`，支持 `|` 运算符。
+  详见 [process.rs](./os/src/syscall/process.rs)
+
+- [x] 重写 `user_shell`，在 `<` `>` 的基础上，支持 `|` 运算符。Refer to [bubble shell](https://github.com/JoshMcguigan/bubble-shell).
+
+  详见 [user_shell.rs](user/src/bin/user_shell.rs)
+
+  简单的测试：
+
+  ```bash
+  hello_world | getchar
+  hello_world | getchar | getchar
+  ```
+
+  或者
+
+  ```bash
+  hello_world > file1
+  cat file1 | getchar
+  ```
 
 ## References
 
--- user/src/bin/user_shell.rs
+++ user/src/bin/user_shell.rs
@@ -14,7 +14,29 @@ const BS: u8 = 0x08u8;
 use alloc::string::String;
 use alloc::vec::Vec;
 use user_lib::console::getchar;
-use user_lib::{close, dup, exec, fork, open, waitpid, OpenFlags, STDIN, STDOUT};
+use user_lib::{close, dup, exec, fork, open, pipe, waitpid, OpenFlags, STDIN, STDOUT};
+
+#[derive(Debug, Clone)]
+enum IOType {
+    File(String),
+    // [read end, write end]
+    Pipe([usize; 2]),
+    Inherit,
+}
+
+impl IOType {
+    pub fn new_pipe() -> Self {
+        let mut pipe_fd: [usize; 2] = [0; 2];
+        pipe(&mut pipe_fd);
+        Self::Pipe(pipe_fd)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Process {
+    pub pid: isize,
+    pub output: IOType,
+}
 
 #[no_mangle]
 pub fn main() -> i32 {
@@ -27,86 +49,128 @@ pub fn main() -> i32 {
             LF | CR => {
                 println!("");
                 if !line.is_empty() {
-                    let args: Vec<_> = line.as_str().split(' ').collect();
-                    let mut args_copy: Vec<String> = args
-                        .iter()
-                        .map(|&arg| {
-                            let mut string = String::new();
-                            string.push_str(arg);
-                            string
-                        })
-                        .collect();
+                    let mut commands = line.trim().split(" | ").peekable();
 
-                    args_copy.iter_mut().for_each(|string| {
-                        string.push('\0');
-                    });
-                    // TODO: 怎么实现 | 运算符?
+                    let mut previous_process: Option<Process> = None;
+                    while let Some(command) = commands.next() {
+                        let args: Vec<_> = command.trim().split(' ').collect();
+                        let mut args_copy: Vec<String> = args
+                            .iter()
+                            .map(|&arg| {
+                                let mut string = String::new();
+                                string.push_str(arg);
+                                string.push('\0');
+                                string
+                            })
+                            .collect();
 
-                    // redirect input
-                    let mut input = String::new();
-                    if let Some((idx, _)) = args_copy
-                        .iter()
-                        .enumerate()
-                        .find(|(_, arg)| arg.as_str() == "<\0")
-                    {
-                        input = args_copy[idx + 1].clone();
-                        args_copy.drain(idx..=idx + 1);
-                    }
+                        // redirect input
+                        let input = if previous_process.is_some() {
+                            previous_process.clone().unwrap().output
+                        } else if let Some((idx, _)) = args_copy
+                            .iter()
+                            .enumerate()
+                            .find(|(_, arg)| arg.as_str() == "<\0")
+                        {
+                            let tmp = IOType::File(args_copy[idx + 1].clone());
+                            args_copy.drain(idx..=idx + 1);
+                            tmp
+                        } else {
+                            IOType::Inherit
+                        };
 
-                    // redirect output
-                    let mut output = String::new();
-                    if let Some((idx, _)) = args_copy
-                        .iter()
-                        .enumerate()
-                        .find(|(_, arg)| arg.as_str() == ">\0")
-                    {
-                        output = args_copy[idx + 1].clone();
-                        args_copy.drain(idx..=idx + 1);
-                    }
+                        // redirect output
+                        let output = if commands.peek().is_some() {
+                            IOType::new_pipe()
+                        } else if let Some((idx, _)) = args_copy
+                            .iter()
+                            .enumerate()
+                            .find(|(_, arg)| arg.as_str() == ">\0")
+                        {
+                            let tmp = IOType::File(args_copy[idx + 1].clone());
+                            args_copy.drain(idx..=idx + 1);
+                            tmp
+                        } else {
+                            IOType::Inherit
+                        };
 
-                    let mut args_addr: Vec<*const u8> =
-                        args_copy.iter().map(|arg| arg.as_ptr()).collect();
-                    args_addr.push(0 as *const u8);
-                    let pid = fork();
-                    if pid == 0 {
-                        // input redirection
-                        if !input.is_empty() {
-                            let input_fd = open(input.as_str(), OpenFlags::RDONLY);
-                            if input_fd == -1 {
-                                println!("Error when opening file {}", input);
+                        let mut args_addr: Vec<*const u8> =
+                            args_copy.iter().map(|arg| arg.as_ptr()).collect();
+                        args_addr.push(0 as *const u8);
+                        let pid = fork();
+                        if pid == 0 {
+                            // input redirection
+                            match input.clone() {
+                                IOType::File(filename) => {
+                                    let input_fd = open(filename.as_str(), OpenFlags::RDONLY);
+                                    if input_fd == -1 {
+                                        println!("Error when opening file {}", filename);
+                                        return -4;
+                                    }
+                                    let input_fd = input_fd as usize;
+                                    close(STDIN);
+                                    assert_eq!(dup(input_fd) as usize, STDIN);
+                                    close(input_fd);
+                                }
+                                IOType::Pipe(pipes) => {
+                                    close(pipes[1]); // close write end
+                                    close(STDIN);
+                                    assert_eq!(dup(pipes[0]) as usize, STDIN);
+                                    close(pipes[0]);
+                                }
+                                _ => {}
+                            }
+                            // output redirection
+                            match output.clone() {
+                                IOType::File(filename) => {
+                                    let output_fd = open(
+                                        filename.as_str(),
+                                        OpenFlags::CREATE | OpenFlags::WRONLY,
+                                    );
+                                    if output_fd == -1 {
+                                        println!("Error when opening file {}", filename);
+                                        return -4;
+                                    }
+                                    let output_fd = output_fd as usize;
+                                    close(STDOUT);
+                                    assert_eq!(dup(output_fd) as usize, STDOUT);
+                                    close(output_fd);
+                                }
+                                IOType::Pipe(pipes) => {
+                                    close(pipes[0]); // close read end
+                                    close(STDOUT);
+                                    assert_eq!(dup(pipes[1]) as usize, STDOUT);
+                                    close(pipes[1]);
+                                }
+                                _ => {}
+                            }
+                            // run child process
+                            if exec(args_copy[0].as_str(), args_addr.as_slice()) == -1 {
+                                println!("Error when executing!");
                                 return -4;
                             }
-                            let input_fd = input_fd as usize;
-                            close(STDIN);
-                            assert_eq!(dup(input_fd), 0);
-                            close(input_fd);
+                            unreachable!("Should not happen!");
                         }
-                        // output redirection
-                        if !output.is_empty() {
-                            let output_fd =
-                                open(output.as_str(), OpenFlags::CREATE | OpenFlags::WRONLY);
-                            if output_fd == -1 {
-                                println!("Error when opening file {}", output);
-                                return -4;
+                        // 旧的管道使用完毕，在此关闭
+                        if let Some(p) = previous_process {
+                            match p.output {
+                                IOType::Pipe(pipes) => {
+                                    close(pipes[0]);
+                                    close(pipes[1]);
+                                }
+                                _ => {}
                             }
-                            let output_fd = output_fd as usize;
-                            close(STDOUT);
-                            assert_eq!(dup(output_fd), 1);
-                            close(output_fd);
                         }
-                        // child process
-                        if exec(args_copy[0].as_str(), args_addr.as_slice()) == -1 {
-                            println!("Error when executing!");
-                            return -4;
-                        }
-                        unreachable!();
-                    } else {
+                        previous_process = Some(Process { pid, output });
+                    }
+                    line.clear();
+                    if let Some(process) = previous_process {
                         let mut exit_code: i32 = 0;
+                        let pid = process.pid;
                         let exit_pid = waitpid(pid as usize, &mut exit_code);
                         assert_eq!(pid, exit_pid);
                         println!("Shell: Process {} exited with code {}", pid, exit_code);
                     }
-                    line.clear();
                 }
                 print!(">> ");
             }
